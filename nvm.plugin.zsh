d519f8249161e32412699a9ac6f305d140ffd34c
João Vitor <jv.zaniolo@gmail.com>
2022-04-15 00:10:16 -0300

    --no-use didnt work

--- nvm.plugin.zsh
+++ nvm.plugin.zsh
@@ -2,7 +2,7 @@
 
 export NVM_DIR="$HOME/.nvm"
 
-[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh --no-use"
+[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"
 [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm"
 
 autoload -U add-zsh-hook
